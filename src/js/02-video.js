cb2000c84e7b02a6066894eedb206522d3548592
Vasiliev781410 <37668858+Vasiliev781410@users.noreply.github.com>
2022-12-22 09:26:22 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+ 
+const player = new Player('vimeo-player', {
+    id: 19231868,
+    width: 640
+});
+
+const onPlay = function(data) {  
+   
+};
+
+
+const saveCurrentTime = function(data) {    
+    localStorage.setItem("videoplayer-current-time", data.seconds);   
+};
+
+player.on('play', onPlay);
+
+player.on('timeupdate', throttle(saveCurrentTime,1000));
+
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
 
